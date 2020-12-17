0efbe0deda6a13fdd2752549a448b28a6cef1871
markassa <markassa@gmail.com>
2020-12-16 22:21:32 -0500

    Start some test cases

---
+++ tests/test-2/expected.js
@@ -0,0 +1,19 @@
+function WINNING_q(V) {
+  let VS = null
+  let PS = null
+  (VS = GETP(dot_V, pq_STRENGTH))
+  (PS = (dot_VS - FIGHT_STRENGTH()))
+  if (G_q(dot_PS, three)) {
+    PROB(nine0)
+  } else if (G_q(dot_PS, zero)) {
+    PROB(seven5)
+  } else if (zero_q(dot_PS)) {
+    PROB(five0)
+  } else if (G_q(dot_VS, one)) {
+    PROB(two5)
+  } else if (true) {
+    PROB(one0)
+  }
+
+}
+
