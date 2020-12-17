a8a57356c258ebf81ff36b2b952f3ce1698467e3
markassa <markassa@gmail.com>
2020-12-16 22:21:05 -0500

    Improve WORD formatting

--- src/js/helpers.rs
+++ src/js/helpers.rs
@@ -10,11 +10,15 @@ pub fn format_string(root: &Node) -> Result<String, ()> {
 }
 
 pub fn format_keyword(root: &Node) -> Result<String, ()> {
-  if !root.is_word() {
-      return Err(());
-  }
+  let (mut keyword, prefix) = crack_keyword(&root)?;
 
-  let keyword = &root.tokens[0].value.replace("-", "_").replace(".", "_2").replace(",", "_3").replace("?", "_q");
+  keyword = match prefix {
+    KeywordWrapper::Pfix => format!("pq_{}", &keyword),
+    KeywordWrapper::Comma => format!("comma_{}", &keyword),
+    KeywordWrapper::Dot => format!("dot_{}", &keyword),
+    KeywordWrapper::None => keyword
+  };
+
   Ok(String::from(keyword))
 }
 
@@ -25,7 +29,7 @@ pub fn crack_keyword(root: &Node) -> Result<(String, KeywordWrapper), ()> {
 
   let mut bare: String;
   let prefix: KeywordWrapper; 
-  let keyword = &root.children[0].tokens[0].value;
+  let keyword = &root.tokens[0].value;
 
   if keyword.starts_with(",P?") {
     bare = keyword[3..].to_string();
@@ -39,6 +43,26 @@ pub fn crack_keyword(root: &Node) -> Result<(String, KeywordWrapper), ()> {
   } else {
     bare = keyword.to_string();
     prefix = KeywordWrapper::None;
+  }
+
+  bare = bare.replace("-", "_");
+
+  let first_char = bare.chars().next().unwrap();
+  if first_char.is_numeric() {
+    let alpha = match first_char {
+      '0' => "zero",
+      '1' => "one",
+      '2' => "two",
+      '3' => "three",
+      '4' => "four",
+      '5' => "five",
+      '6' => "six",
+      '7' => "seven",
+      '8' => "eight",
+      '9' => "nine",
+      _ => return Err(())
+    };
+    bare = format!("{}{}", alpha, &bare[1..]);
   }
 
   if bare.ends_with("?") {
